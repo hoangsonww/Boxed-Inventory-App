9ab9c6e1fe6f4f6333d309485964473b63ebbf8b
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:04 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'Boxed-Inventory-App';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
